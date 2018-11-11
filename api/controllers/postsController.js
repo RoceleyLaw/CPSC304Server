ac7295285431ae7aae61a34d9f6904332697b2cb
Roceley Luo <roceleylaw@gmail.com>
2018-11-11 14:41:56 -0800

    put edited

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -22,10 +22,10 @@ var PostsController={
     updatePostByID:function(id, newPost, callback){
         console.log(newPost)
         // As id is the primary key so the array has only one value
-        const Post = newPost[0];
+        const Post = newPost;
         const updateSql =
                 "UPDATE cpsc304.PostedRealEstate SET listedPrice=?, postalCode=?, pictureURL=?, bedroom=?, bathroom=?, licenseNumber=? WHERE listingID=?"
-        console.log('this is the post', Post.listedPrice);
+        console.log('this is the post', Post);
         return con.query(updateSql, [Post.listedPrice, Post.postalCode, Post.pictureURL, Post.bedroom, Post.bathroom, Post.licenseNumber, Post.listingID], callback)
     },
     
@@ -36,6 +36,22 @@ var PostsController={
 
     getAllApts:function(callback){
         return con.query("SELECT * FROM cpsc304.Apartments", callback)
+    },
+
+    addNewHouse:function(House, callback){
+        const insertSql =
+                "INSERT INTO cpsc304.PostedRealEstate(listingID, listedPrice, postalCode, pictureURL, bedroom, bathroom, licenseNumber)\
+                 values (?,?,?,?,?,?,?);\
+                 INSERT INTO cpsc304.Houses(listingID, houseNumber, lotSize)\
+                 values (?,?,?);"
+                
+        return con.query(insertSql,[House.listingID, House.listedPrice, House.postalCode, House.pictureURL, House.bedroom, House.bathroom, House.licenseNumber],callback);
+    },
+
+    addNewApt:function(Apt, callback){
+        console.log(Post);
+        return con.query("INSERT INTO cpsc304.PostedRealEstate(listingID, listedPrice, postalCode, pictureURL, bedroom, bathroom, licenseNumber) values (?,?,?,?,?,?,?)",[Post.listingID, Post.listedPrice, Post.postalCode, Post.pictureURL, Post.bedroom, Post.bathroom, Post.licenseNumber],callback);
     }
+
 };
 module.exports=PostsController;
