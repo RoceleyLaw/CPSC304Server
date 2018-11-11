f48d52c87048f1bb54c909d84eb4801af213c43c
Roceley Luo <roceleylaw@gmail.com>
2018-11-11 14:02:19 -0800

    access control changed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
