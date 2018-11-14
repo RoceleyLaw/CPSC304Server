edf90896be6fb56472b0c3ca146202bb23c98c61
Roceley Luo <roceleylaw@gmail.com>
2018-11-13 20:25:47 -0800

    added soldlistingcontroller

---
+++ SQL/SoldListings.sql
@@ -0,0 +1,2 @@
+insert into SoldListings(agreementNumber, finalPrice, soldDate, completionDate, phoneNumber, licenseNumber, listingID) values (null,100000,'2018-02-14','2018-11-14','1024360825', '1371983','39720');
+insert into SoldListings(agreementNumber, finalPrice, soldDate, completionDate, phoneNumber, licenseNumber, listingID) values (null,2000000,'2018-02-14','2018-11-14','1024360825', '1371983','97220');
